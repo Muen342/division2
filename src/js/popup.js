f98b570e3db995247a9d990b10ebd61f96fd270d
mzhang <muen2001@hotmail.com>
2021-01-16 15:34:02 -0500

    back to normal

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,23 @@
-import "../css/popup.css";
-import hello from "./popup/example";
+var alarmClock = {
 
-hello();
+        onHandler : function(e) {
+            chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.2} );
+                    window.close();
+        },
+
+        offHandler : function(e) {
+            chrome.alarms.clear("myAlarm");
+                    window.close();
+        },
+
+        setup: function() {
+            var a = document.getElementById('alarmOn');
+            a.addEventListener('click',  alarmClock.onHandler );
+            var a = document.getElementById('alarmOff');
+            a.addEventListener('click',  alarmClock.offHandler );
+        }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    alarmClock.setup();
+});
