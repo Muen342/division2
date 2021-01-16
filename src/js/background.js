1686e01f6d169e1b5293402cc8d259783e1ae72c
mzhang <muen2001@hotmail.com>
2021-01-16 17:23:45 -0500

    fixed popup

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,12 @@
 import '../img/icon-128.png';
 import '../img/icon-34.png';
 
+
 chrome.alarms.onAlarm.addListener(buttonClicked);
 
 function buttonClicked(alarm){
     chrome.windows.create({
-        url: chrome.runtime.getURL("display.html"),//chrome.extension.getURL("display.html"),
-        type: "popup",
-        window: "locked fullscreen"}
+        url: "background.html"}
         );
     alert("Beep");
 }
