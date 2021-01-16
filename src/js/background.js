6107cf5439f9f4b0a1c73ce4a2ad7c03ee311202
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2021-01-16 15:16:48 -0500

    alarm

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,19 @@
-import '../img/icon-128.png'
-import '../img/icon-34.png'
+
+chrome.alarms.onAlarm.addListener(buttonClicked);
+
+// chrome.alarms.onAlarm.addListener(function(alarm) {
+//   //alert("Beep");
+//   console.log("hello");
+// });
+
+
+function buttonClicked(alarm){
+    alert("Beep");
+}
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, msg);
+// }
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"});
+// });
