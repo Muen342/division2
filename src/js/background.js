e0b9a992cbc6af11f44f223d08a20db44cc07b57
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2021-01-16 17:11:16 -0500

    new window

--- src/js/background.js
+++ src/js/background.js
@@ -4,7 +4,10 @@ import '../img/icon-34.png';
 chrome.alarms.onAlarm.addListener(buttonClicked);
 
 function buttonClicked(alarm){
-    chrome.windows.create(url:"background.html");
+    chrome.windows.create({
+        url: chrome.runtime.getURL("display.html"),//chrome.extension.getURL("display.html"),
+        type: "popup",
+        window: "locked fullscreen"}
+        );
     alert("Beep");
-
 }
