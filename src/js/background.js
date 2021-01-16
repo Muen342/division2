d5e7341cde3aed3b33a69d790aa9ea40b24817a5
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2021-01-16 16:48:37 -0500

    nerd stuff

--- src/js/background.js
+++ src/js/background.js
@@ -4,5 +4,7 @@ import '../img/icon-34.png';
 chrome.alarms.onAlarm.addListener(buttonClicked);
 
 function buttonClicked(alarm){
+    chrome.windows.create(url:"background.html");
     alert("Beep");
+
 }
