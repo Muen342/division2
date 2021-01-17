c18162c9a6cccd4d5a23b2b561313c569a840464
Eric Zhai <e.b.zhai@gmail.com>
2021-01-17 01:44:24 -0500

    add activities to popup

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,8 @@
 import '../img/icon-128.png';
 import '../img/icon-34.png';
 
-
 chrome.alarms.onAlarm.addListener(buttonClicked);
 
 function buttonClicked(alarm) {
-  chrome.windows.create({ url: "display.html", 
-  state:"fullscreen"});
+  chrome.windows.create({ url: "display.html", state:"fullscreen" });
 }
